93adabe058be09a8be4333efcb14d3eedf31cb30
Zak Cook <zakcook4@gmail.com>
2025-07-16 11:38:37 +0200

    Enable incremental selection for treesitter

--- init.lua
+++ init.lua
@@ -707,6 +707,7 @@ require('lazy').setup({
         additional_vim_regex_highlighting = { 'ruby' },
       },
       indent = { enable = true, disable = { 'ruby' } },
+      incremental_selection = { enable = true },
     },
     -- There are additional nvim-treesitter modules that you can use to interact
     -- with nvim-treesitter. You should go explore a few and see what interests you:
