4b189da67f8b8b00b4f6478de56ff45de77c8f98
Zak Cook <zakcook4@gmail.com>
2026-09-07 00:29:50 +0200

    markdown format on save with dprint

--- init.lua
+++ init.lua
@@ -787,6 +787,7 @@ do
       local enabled_filetypes = {
         -- lua = true,
         -- python = true,
+        markdown = true,
       }
       if enabled_filetypes[vim.bo[bufnr].filetype] then
         return { timeout_ms = 500 }
@@ -799,6 +800,7 @@ do
     },
     -- You can also specify external formatters in here.
     formatters_by_ft = {
+      markdown = {'dprint'},
       -- rust = { 'rustfmt' },
       -- Conform can also run multiple formatters sequentially
       -- python = { "isort", "black" },
