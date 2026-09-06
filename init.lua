a93a63e4473ed0146273a2939742730b5ca88a4b
Zak Cook <zakcook4@gmail.com>
2026-09-06 17:17:49 +0200

    switch theme to melange, enable neotree and autopairs

--- init.lua
+++ init.lua
@@ -423,18 +423,27 @@ do
   -- change the command under that to load whatever the name of that colorscheme is.
   --
   -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-  vim.pack.add { gh 'folke/tokyonight.nvim' }
-  ---@diagnostic disable-next-line: missing-fields
-  require('tokyonight').setup {
-    styles = {
-      comments = { italic = false }, -- Disable italics in comments
-    },
-  }
+  vim.pack.add { gh 'savq/melange-nvim' }
+
+  vim.api.nvim_create_autocmd("ColorScheme", {
+  pattern = "melange",
+  callback = function()
+    local groups = {
+      "Normal", "NormalNC",           -- background
+      "WinSeparator", "VertSplit",    -- line separators
+      "NeoTreeNormal", "NeoTreeNormalNC", "NeoTreeEndOfBuffer", -- neo-tree
+      "TelescopeNormal", "TelescopeBorder", "TelescopePromptNormal", -- telescope
+    }
+    for _, group in ipairs(groups) do
+      vim.api.nvim_set_hl(0, group, { bg = "none" })
+    end
+  end,
+})
 
   -- Load the colorscheme here.
   -- Like many other themes, this one has different styles, and you could load
   -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-  vim.cmd.colorscheme 'tokyonight-night'
+  vim.cmd.colorscheme 'melange'
 
   -- Highlight todo, notes, etc in comments
   vim.pack.add { gh 'folke/todo-comments.nvim' }
@@ -988,8 +997,8 @@ do
   -- require 'kickstart.plugins.debug'
   -- require 'kickstart.plugins.indent_line'
   -- require 'kickstart.plugins.lint'
-  -- require 'kickstart.plugins.autopairs'
-  -- require 'kickstart.plugins.neo-tree'
+  require 'kickstart.plugins.autopairs'
+  require 'kickstart.plugins.neo-tree'
 
   -- NOTE: You can add your own plugins, configuration, etc. in `lua/custom/plugins/*.lua`.
   --
